d77322f4c9b5e57f31d9d64a054eb61c36cc9279
thomas <thomas.chiantia@gmail.com>
2020-05-06 09:21:58 -0400

    unused babels

---
+++ babel.config.js
@@ -0,0 +1,15 @@
+/*
+module.exports = {
+  env: {
+    production: {
+      plugins: ['@babel/plugin-transform-modules-commonjs']
+    },
+    development: {
+      plugins: ['@babel/plugin-transform-modules-commonjs']
+    },
+    test: {
+      plugins: ['@babel/plugin-transform-modules-commonjs']
+    }
+  }
+};
+*/
