ab30707f18d99965c91086d7714abc3558fa5e99
thomas <thomas.chiantia@gmail.com>
2020-05-06 11:51:39 -0400

    removed unused

--- jest.config.js
+++ jest.config.js
@@ -3,9 +3,5 @@ module.exports = {
   coverageDirectory: 'coverage',
   setupFiles: ['./src/components/__tests__/setup.ts'],
   testEnvironment: 'jsdom',
-  // transform: {
-  //   '^.+\\.js$': 'babel-jest'
-  // },
-  // transformIgnorePatterns: [], // default ["node_modules"]
   testMatch: ['**/?(*.)+(spec|test).[t]s?(x)']
 };
