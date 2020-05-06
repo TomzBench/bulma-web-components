1de95fbde21215bd0e416c307d5c286bda93a2af
thomas <thomas.chiantia@gmail.com>
2020-05-06 14:26:50 -0400

    removing jest, adding karma

--- jest.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  preset: 'ts-jest',
-  coverageDirectory: 'coverage',
-  setupFiles: ['./src/components/__tests__/setup.ts'],
-  testEnvironment: 'jsdom',
-  testMatch: ['**/?(*.)+(spec|test).[t]s?(x)']
-};
