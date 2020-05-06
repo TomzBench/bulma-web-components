f601306d2f5bdec558cf52617740828bffe7623e
thomas <thomas.chiantia@gmail.com>
2020-05-06 11:11:38 -0400

    file add

---
+++ src/components/table-user/__tests__/table-user.test.ts
@@ -0,0 +1,7 @@
+// import '../table-user';
+// import { AtxUserTable } from '../table-user';
+
+test('table-user should render', () => {
+  // let table = new AtxUserTable();
+  expect(true).toBeTruthy();
+});
