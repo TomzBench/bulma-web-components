eddb885dcb7bcf4f679b478c001d3881e1f55bbd
thomas <thomas.chiantia@gmail.com>
2020-05-11 11:36:00 -0400

    ...

---
+++ src/services/router/__tests__/router.service.test.ts
@@ -0,0 +1,10 @@
+import 'reflect-metadata'; // TODO why is this not imported already
+import { parse } from '../router.service';
+import sinon from 'sinon';
+
+describe('router.service', () => {
+  it('should parse', () => {
+    let result = parse('/foo/:bar/abcd/users/:user');
+    console.log(result);
+  });
+});
