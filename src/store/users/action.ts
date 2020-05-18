a1844f9c78cea1157776dc1da5c4faf948832390
thomas <thomas.chiantia@gmail.com>
2020-05-18 17:05:23 -0400

    redux boilerplate

--- src/store/users/action.ts
+++ src/store/users/action.ts
@@ -19,19 +19,41 @@ export interface Fetch extends Action<typeof FETCH> {
   start: number;
   limit: number;
 }
+export interface FetchOk extends User, Action<typeof FETCH_OK> {
+  users: User[];
+}
+export interface FetchErr extends Action<typeof FETCH_ERR> {}
 
 // Submit login for authentication
 export interface Login extends Action<typeof LOGIN> {
   email: string;
   password: string;
 }
-
 export interface LoginOk extends User, Action<typeof LOGIN_OK> {}
+export interface LoginErr extends Action<typeof LOGIN_ERR> {}
+
+// Logout actions
 export interface Logout extends Action<typeof LOGOUT> {}
+export interface LogoutOk extends Action<typeof LOGOUT_OK> {}
+export interface LogoutErr extends Action<typeof LOGOUT_ERR> {}
 
 export const fetch = actionCreator<Fetch>(FETCH);
+export const fetchOk = actionCreator<FetchOk>(FETCH_OK);
+export const fetchErr = actionCreator<FetchErr>(FETCH_ERR);
 export const login = actionCreator<Login>(LOGIN);
 export const loginOk = actionCreator<LoginOk>(LOGIN_OK);
+export const loginErr = actionCreator<LoginErr>(LOGIN_ERR);
 export const logout = actionCreator<Logout>(LOGOUT);
+export const logoutOk = actionCreator<LogoutOk>(LOGOUT_OK);
+export const logoutErr = actionCreator<LogoutErr>(LOGOUT_ERR);
 
-export type Actions = Fetch | Login | Logout;
+export type Actions =
+  | Fetch
+  | FetchErr
+  | FetchOk
+  | Login
+  | LoginOk
+  | LoginErr
+  | Logout
+  | LogoutOk
+  | LogoutErr;
--- src/store/users/reducer.ts
+++ src/store/users/reducer.ts
@@ -6,11 +6,29 @@ export default function reducer(state = initial, action: Action.Actions) {
     case Action.FETCH:
       return fetch(state, action);
       break;
+    case Action.FETCH_OK:
+      return fetchOk(state, action);
+      break;
+    case Action.FETCH_ERR:
+      return fetchErr(state, action);
+      break;
     case Action.LOGIN:
       return login(state, action);
       break;
+    case Action.LOGIN_OK:
+      return loginOk(state, action);
+      break;
+    case Action.LOGIN_ERR:
+      return loginErr(state, action);
+      break;
     case Action.LOGOUT:
       return logout(state, action);
+      break;
+    case Action.LOGOUT_OK:
+      return logoutOk(state, action);
+      break;
+    case Action.LOGOUT_ERR:
+      return logoutErr(state, action);
       break;
   }
 }
@@ -19,10 +37,34 @@ function fetch(state: State, action: Action.Fetch): State {
   return { ...state };
 }
 
+function fetchOk(state: State, action: Action.FetchOk): State {
+  return { ...state };
+}
+
+function fetchErr(state: State, action: Action.FetchErr): State {
+  return { ...state };
+}
+
 function login(state: State, action: Action.Login): State {
-  return state;
+  return { ...state };
+}
+
+function loginOk(state: State, action: Action.LoginOk): State {
+  return { ...state };
+}
+
+function loginErr(state: State, action: Action.LoginErr): State {
+  return { ...state };
 }
 
 function logout(state: State, action: Action.Logout): State {
-  return state;
+  return { ...state };
+}
+
+function logoutOk(state: State, action: Action.LogoutOk): State {
+  return { ...state };
+}
+
+function logoutErr(state: State, action: Action.LogoutErr): State {
+  return { ...state };
 }
